39bfb62baaef1302d11fd306af8e6bb6fe752df3
Konsta Kuosmanen <contact@norpie.dev>
2024-04-29 19:46:17 +0200

    fix: last debug broke

--- install.sh
+++ install.sh
@@ -100,8 +100,10 @@ function hidden_prompt() {
 
 function cleanup() {
     if [ -d /mnt ]; then
+        normal "Unmounting /mnt"
         #umount -R /mnt
     fi
+    normal "Disabling swap"
     # swapoff "${disk}2"
     nix-store --gc
 }
