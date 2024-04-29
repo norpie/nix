de9b9fd240f3b1a0d812c253aa90cdfacaf1e872
Konsta Kuosmanen <contact@norpie.dev>
2024-04-29 23:13:21 +0200

    fix: dwm autologin

--- hosts/common/optional/desktops/dwm-env.nix
+++ hosts/common/optional/desktops/dwm-env.nix
@@ -1,25 +1,48 @@
-{ pkgs, configLib, ... }: {
+{
+  pkgs,
+  configLib,
+  ...
+}: {
   imports = [
     (configLib.relativeToRoot "hosts/common/optional/fonts.nix")
     (configLib.relativeToRoot "hosts/common/optional/apps/dmenu.nix")
     (configLib.relativeToRoot "hosts/common/optional/apps/st.nix")
   ];
 
-  services.xserver = {
-    enable = true;
-    autorun = true;
-    xkb = {
-      layout = "us";
-      variant = "";
+  services = {
+    displayManager = {
+      autoLogin = {
+        enable = true;
+        user = "norpie";
+      };
     };
-    windowManager.dwm = {
+    xserver = {
+      displayManager = {
+        lightdm = {
+          enable = true;
+          greeter = {
+            enable = true;
+            name = "lightdm-webkit2-greeter";
+          };
+        };
+      };
       enable = true;
-      package = pkgs.dwm.overrideAttrs {
-        src = pkgs.fetchgit {
-          name = "dwm";
-          url = "https://github.com/norman-pkgs/dwm";
-          rev = "07fe725120aded9a281f69d4ed8643acd1f442dc";
-          sha256 = "sha256-l65iJ6NMIrorSViPI/myf+gaFTUfU4dHkqLcxR/z7sE=";
+      autorun = true;
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+      windowManager = {
+        dwm = {
+          enable = true;
+          package = pkgs.dwm.overrideAttrs {
+            src = pkgs.fetchgit {
+              name = "dwm";
+              url = "https://github.com/norman-pkgs/dwm";
+              rev = "07fe725120aded9a281f69d4ed8643acd1f442dc";
+              sha256 = "sha256-l65iJ6NMIrorSViPI/myf+gaFTUfU4dHkqLcxR/z7sE=";
+            };
+          };
         };
       };
     };
@@ -46,6 +69,4 @@
     xwallpaper
     numlockx
   ];
-
-  services.getty.autologinUser = "norpie";
 }
