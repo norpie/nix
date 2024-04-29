d2fff22deb77a1d899db5ca359cb8d606c88b21e
Konsta Kuosmanen <contact@norpie.dev>
2024-04-29 21:48:22 +0200

    feat: root password in config

--- hosts/common/users/norpie.nix
+++ hosts/common/users/norpie.nix
@@ -6,5 +6,10 @@
     hashedPassword = "$6$yrmuKldtP/mKYReB$9w8pTeEmK/oY2xnY2mG5Wwj6N72qoeoYp.zIHpPS7/inQrR70LU0dNvjUFc0hTFvS9BcwUV9XY2.girsF8JyN.";
   };
 
+  users.users.root = {
+    isNormalUser = false;
+    hashedPassword = "$6$yrmuKldtP/mKYReB$9w8pTeEmK/oY2xnY2mG5Wwj6N72qoeoYp.zIHpPS7/inQrR70LU0dNvjUFc0hTFvS9BcwUV9XY2.girsF8JyN.";
+  };
+
   security.sudo.wheelNeedsPassword = false;
 }
