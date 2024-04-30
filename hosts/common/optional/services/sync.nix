d950f03a31751693ec1411b6a0022d5dbfb3a8c8
Konsta Kuosmanen <contact@norpie.dev>
2024-04-30 11:24:20 +0200

    fix: trying to fix syncthing

--- hosts/common/optional/services/sync.nix
+++ hosts/common/optional/services/sync.nix
@@ -6,6 +6,13 @@
       user = "norpie";
       dataDir = "/home/norpie"; # Default folder for new synced folders
       configDir = "/home/norpie/.config/syncthing"; # Folder for Syncthing's settings and keys
+      settings = {
+        gui = {
+          address = "0.0.0.0:8384";
+          theme = "dark";
+          password = "password";
+        };
+      };
     };
   };
 }
