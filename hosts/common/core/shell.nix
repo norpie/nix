a4e12278c1c97ab78bc7b224857e0e59403d55f1
Konsta Kuosmanen <contact@norpie.dev>
2024-05-01 13:43:17 +0200

    feat: add local bin to path

--- hosts/common/core/shell.nix
+++ hosts/common/core/shell.nix
@@ -1,8 +1,14 @@
-{ pkgs, ... }: {
-    users.defaultUserShell = pkgs.zsh;
-    environment.variables = {ZDOTDIR = "/home/norpie/.config/zsh";};
-    programs.zsh = {enable = true;};
-    environment.systemPackages = with pkgs; [
-        fzf
-    ];
+{pkgs, ...}: {
+  users.defaultUserShell = pkgs.zsh;
+  environment.variables = {
+    ZDOTDIR = "/home/norpie/.config/zsh";
+  };
+  environment.localBinInPath = true;
+  environment.pathsToLink = [
+    "/home/norpie/.local/bin/"
+  ];
+  programs.zsh = {enable = true;};
+  environment.systemPackages = with pkgs; [
+    fzf
+  ];
 }
