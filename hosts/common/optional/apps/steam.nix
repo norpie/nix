a33cc7913f10930aa67749b93f6d1cd3b151c954
Konsta Kuosmanen <contact@norpie.dev>
2024-05-01 22:05:53 +0200

    feat: steam/gaming improvements

--- hosts/common/optional/apps/steam.nix
+++ hosts/common/optional/apps/steam.nix
@@ -1,10 +1,24 @@
-{ ... }: {
-    programs.steam = {
-        enable = true;
-        package = pkgs.steam-small.override {
-            extraEnv = {
-                HOME = "~/.local/data/steam";
-            }
+{pkgs, ...}: {
+  programs = {
+    gamemode.enable = true;
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+      package = pkgs.steam-small {
+        extraEnv = {
+          HOME = "~/.local/data/steam";
         };
-    }
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    bottles
+    lutris
+    protonup
+  ];
+
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/norpie/.local/data/steam/.steam/root/compatibilitytools.d";
+  };
 }
