3a73eb5a593f22bbfc760b5045c050cacc2f2af3
Konsta Kuosmanen <contact@norpie.dev>
2024-05-01 17:58:30 +0200

    feat: enable wireplumber

--- hosts/common/optional/services/audio.nix
+++ hosts/common/optional/services/audio.nix
@@ -1,23 +1,35 @@
 {pkgs, ...}: {
-  # Enable sound with pipewire.
   sound.enable = true;
+
   hardware.pulseaudio.enable = false;
+
   security.rtkit.enable = true;
+
   services.pipewire = {
     enable = true;
+
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
   };
   environment.systemPackages = with pkgs; [
     pulseaudio
     pamixer
     pavucontrol
+  ];
+
+  # Wireplumber bluetooth config
+  services.pipewire.wireplumber.configPackages = [
+    (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
+      bluez_monitor.properties = {
+              ["bluez5.enable-sbc-xq"] = true,
+              ["bluez5.enable-msbc"] = true,
+              ["bluez5.codecs"] = "[ sbc_xq ]",
+              ["bluez5.roles"] = "[ a2dp_sink ]",
+              ["bluez5.hfphsp-backend"] = "none"
+      }
+    '')
   ];
 }
