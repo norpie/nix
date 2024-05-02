caa1d949b9efa2d81dab810e5fd2bfe3476e90f2
Konsta Kuosmanen <contact@norpie.dev>
2024-05-02 20:19:46 +0200

    fix: added trying to make more drivers work

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -43,6 +43,8 @@
 
   services.xserver.videoDrivers = [ "intel" ];
 
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
+
   hardware.opengl.extraPackages = with pkgs; [
     intel-media-driver
     intel-ocl
