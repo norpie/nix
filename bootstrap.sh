3c76ab21a3885aa550468019d6a7c6bc8096734c
Konsta Kuosmanen <contact@norpie.dev>
2024-05-02 16:33:52 +0200

    fix: new way of doing the ascii

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,8 @@
 # nix-shell -p git --command "nix run --experimental-features 'nix-command flakes' github:norpie/nix"
 # we need to echo the command above into install.sh
 
-# ASCII art that says to "run ./install.sh"
-ascii="
-                          _ __   _   _   _ __                           \n
-                         | '__| | | | | | '_ \                          \n
-                         | |    | |_| | | | | |                         \n
-                         |_|     \__,_| |_| |_|                         \n
-
-         __  _                 _             _   _             _        \n
-        / / (_)  _ __    ___  | |_    __ _  | | | |      ___  | |__     \n
-       / /  | | | '_ \  / __| | __|  / _\` | | | | |     / __| | '_ \   \n
-  _   / /   | | | | | | \__ \ | |_  | (_| | | | | |  _  \__ \ | | | |   \n
- (_) /_/    |_| |_| |_| |___/  \__|  \__,_| |_| |_| (_) |___/ |_| |_|   \n
-
-run ./install.sh
-"
 echo "nix-shell -p git --command \"nix run --experimental-features 'nix-command flakes' github:norpie/nix\"" >install.sh &&
     chmod +x install.sh &&
-    echo $ascii
+    base64 -d <<<"H4sIAAAAAAAAA5VQQQrDMAy75xW6dYNCPhTQHpLH17LTxiMbpQ5JkIVtWcDPKABBQt95BQs6NrJj
+no1onu8qtC+zxaGikTsHS8v/nTtC07jwXNCaSV2qnRff1zL2MjSOEYFO8Ynn7KImeVeD4YBegU9y
+IwqCH864KG+C7JsZYnRo6aYycyq5+PBRa1RW93j6KKVKNtfy7bGXBH/j+rMoB5W6p4U0AgAA" | gunzip
