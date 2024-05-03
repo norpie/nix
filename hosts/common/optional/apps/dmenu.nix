39fd7eceba5637dddf40bca376228a071d65e561
Konsta Kuosmanen <contact@norpie.dev>
2024-05-03 21:00:09 +0200

    fix: no more norman-pkgs github org

--- hosts/common/optional/apps/dmenu.nix
+++ hosts/common/optional/apps/dmenu.nix
@@ -3,7 +3,7 @@
     (dmenu.overrideAttrs (oldAttrs: {
       src = fetchgit {
         name = "dmenu";
-        url = "https://github.com/norman-pkgs/dmenu";
+        url = "https://github.com/norpie/dmenu";
         rev = "6417bade26a880fa777cb1c8f185af2feb607135";
         sha256 = "sha256-Zh3wjJ7eQV9h6gmQG9o7I8WOv9Kk7/xUIE2As5qJG/I=";
         };
