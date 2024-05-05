3b5f73f7e6f24abdcb82a91f1ac17b567b0548df
Konsta Kuosmanen <contact@norpie.dev>
2024-05-05 20:08:47 +0200

    fix: temporaly disable virt

--- hosts/common/optional/virtualization.nix
+++ hosts/common/optional/virtualization.nix
@@ -1,38 +1,38 @@
 {pkgs, ...}: {
-  boot.extraModprobeConfig = "options kvm_amd nested=1";
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = true;
-      swtpm = {
-        enable = true;
-      };
-      ovmf = {
-        enable = true;
-        packages = [
-          (
-            pkgs.unstable.OVMF.override {
-              secureBoot = true;
-              tpmSupport = true;
-            }
-          )
-          .fd
-        ];
-      };
-    };
-  };
-  programs.virt-manager = {
-    enable = true;
-  };
-  environment = {
-    systemPackages = with pkgs; [
-      quickemu
-      (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
-        qemu-system-x86_64 \
-          -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
-          "$@"
-      '')
-    ];
-  };
+  # boot.extraModprobeConfig = "options kvm_amd nested=1";
+  # virtualisation.libvirtd = {
+  #   enable = true;
+  #   qemu = {
+  #     package = pkgs.qemu_kvm;
+  #     runAsRoot = true;
+  #     swtpm = {
+  #       enable = true;
+  #     };
+  #     ovmf = {
+  #       enable = true;
+  #       packages = [
+  #         (
+  #           pkgs.unstable.OVMF.override {
+  #             secureBoot = true;
+  #             tpmSupport = true;
+  #           }
+  #         )
+  #         .fd
+  #       ];
+  #     };
+  #   };
+  # };
+  # programs.virt-manager = {
+  #   enable = true;
+  # };
+  # environment = {
+  #   systemPackages = with pkgs; [
+  #     quickemu
+  #     (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
+  #       qemu-system-x86_64 \
+  #         -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
+  #         "$@"
+  #     '')
+  #   ];
+  # };
 }
