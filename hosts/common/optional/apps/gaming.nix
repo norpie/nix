3158659f1b2b7e17f7720e68014d4456298d1e22
Konsta Kuosmanen <contact@norpie.dev>
2024-05-06 16:12:19 +0200

    doc: added TODO for steamg

--- hosts/common/optional/apps/gaming.nix
+++ hosts/common/optional/apps/gaming.nix
@@ -1,4 +1,6 @@
 {pkgs, ...}: {
+  # TODO: This v
+  # https://github.com/mateusauler/nixos-config/blob/9234534867e50cef659997c67a5b71eb84e28670/home-manager-modules/scripts/steam-xdg.nix
   programs = {
     gamemode.enable = true;
     steam = {
