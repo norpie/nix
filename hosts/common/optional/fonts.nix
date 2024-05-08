85d164ec57ebc3ab90f3c555e64769504edd0665
Konsta Kuosmanen <contact@norpie.dev>
2024-05-08 16:22:19 +0200

    feat: more fonts

--- hosts/common/optional/fonts.nix
+++ hosts/common/optional/fonts.nix
@@ -1,10 +1,32 @@
 {pkgs, ...}: {
   nixpkgs.config.joypixels.acceptLicense = true;
-  fonts.packages = with pkgs; [
-    source-sans-pro
-    nerdfonts
-    joypixels
-    lmodern
-    lmmath
-  ];
+  fonts = {
+    packages = with pkgs; [
+      material-icons
+      material-design-icons
+      roboto
+      work-sans
+      comic-neue
+      source-sans
+      twemoji-color-font
+      comfortaa
+      inter
+      lato
+      lexend
+      jost
+      dejavu_fonts
+      iosevka-bin
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      jetbrains-mono
+      source-sans-pro
+      jetbrains-mono
+      ubuntu_font_family
+      nerdfonts
+      joypixels
+      lmodern
+      lmmath
+    ];
+  };
 }
