36ff707c1dfee1864f9bb29456ee01417b45bf50
Konsta Kuosmanen <contact@norpie.dev>
2024-05-11 15:43:33 +0200

    fix: samba

--- hosts/common/optional/services/nas.nix
+++ hosts/common/optional/services/nas.nix
@@ -1,43 +1,43 @@
 {...}: {
   services = {
-    samba-wsdd = {
-      enable = true;
-      interface = "virbr0";
-      discovery = true;
-    };
+    # samba-wsdd = {
+    #   enable = true;
+    #   interface = "virbr0";
+    #   discovery = true;
+    # };
     samba = {
       enable = true;
       extraConfig = ''
-        server string = nas
-        hosts allow = 192.168.0. 127.0.0.1 localhost
+        workgroup = WORKGROUP
+        server role = standalone server
       '';
       shares = {
         data = {
           comment = "Data drive";
           path = "/mnt/data";
-          validUsers = ["norpie"];
+          "valid users" = "norpie";
           public = false;
           writable = true;
           printable = false;
-          createMask = "0777";
+          "create mask" = "0765";
         };
         media = {
           comment = "Media drive";
           path = "/mnt/media";
-          validUsers = ["norpie"];
+          "valid users" = "norpie";
           public = false;
           writable = true;
           printable = false;
-          createMask = "0765";
+          "create mask" = "0765";
         };
         home = {
           comment = "Home dir";
           path = "/home/norpie";
-          validUsers = ["norpie"];
+          "valid users" = "norpie";
           public = false;
           writable = true;
           printable = false;
-          createMask = "0765";
+          "create mask" = "0765";
         };
       };
     };
