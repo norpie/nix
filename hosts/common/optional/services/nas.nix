a7785806ff4a073c5c3b6257c5a7d2194d226d1e
Konsta Kuosmanen <contact@norpie.dev>
2024-05-11 12:57:45 +0200

    feat: samba discovery

--- hosts/common/optional/services/nas.nix
+++ hosts/common/optional/services/nas.nix
@@ -1,71 +1,45 @@
 {...}: {
-  services.samba = {
-    enable = true;
-    shares = {
-      data = {
-        comment = "Data drive";
-        path = "/mnt/data";
-        validUsers = ["norpie"];
-        public = false;
-        writable = true;
-        printable = false;
-        createMask = "0777";
-      };
-      media = {
-        comment = "Media drive";
-        path = "/mnt/media";
-        validUsers = ["norpie"];
-        public = false;
-        writable = true;
-        printable = false;
-        createMask = "0765";
-      };
-      home = {
-        comment = "Home dir";
-        path = "/home/norpie";
-        validUsers = ["norpie"];
-        public = false;
-        writable = true;
-        printable = false;
-        createMask = "0765";
+  services = {
+    samba-wsdd = {
+      enable = true;
+      interface = "virbr0";
+      discovery = true;
+    };
+    samba = {
+      enable = true;
+      extraConfig = ''
+        server string = nas
+        hosts allow = 192.168.0. 127.0.0.1 localhost
+      '';
+      shares = {
+        data = {
+          comment = "Data drive";
+          path = "/mnt/data";
+          validUsers = ["norpie"];
+          public = false;
+          writable = true;
+          printable = false;
+          createMask = "0777";
+        };
+        media = {
+          comment = "Media drive";
+          path = "/mnt/media";
+          validUsers = ["norpie"];
+          public = false;
+          writable = true;
+          printable = false;
+          createMask = "0765";
+        };
+        home = {
+          comment = "Home dir";
+          path = "/home/norpie";
+          validUsers = ["norpie"];
+          public = false;
+          writable = true;
+          printable = false;
+          createMask = "0765";
+        };
       };
     };
   };
-  /*
-    [data]
-     comment = Data drive
-     path = /mnt/data
-     valid users = norpie
-     public = no
-     writable = yes
-     printable = no
-     create mask = 0777
-
-  [media]
-     comment = Media drive
-     path = /mnt/media
-     valid users = norpie
-     public = no
-     writable = yes
-     printable = no
-     create mask = 0765
-
-  [home]
-     comment = Home dir
-     path = /home/norpie
-     valid users = norpie
-     public = no
-     writable = yes
-     printable = no
-     create mask = 0765
-
-  [ssd]
-     comment = SSD Drive
-     path = /mnt/ssd
-     valid users = norpie
-     public = no
-     writable = yes
-     printable = no
-     create mask = 0765
-  */
 }
