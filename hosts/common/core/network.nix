91913ed1376a252360742a5885115e3a2f4f8e80
Konsta Kuosmanen <contact@norpie.dev>
2024-05-11 12:57:27 +0200

    feat: disabled firewall

--- hosts/common/core/network.nix
+++ hosts/common/core/network.nix
@@ -1,5 +1,6 @@
 {...}: {
   networking.networkmanager.enable = true;
+  networking.firewall.enable = false;
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -8,5 +9,4 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 }
