f1882750fd8acbbe72adc06b9e4fedeb800bbaa9
Konsta Kuosmanen <contact@norpie.dev>
2024-05-14 14:40:45 +0200

    feat: added ssh authorized key

--- hosts/common/users/norpie.nix
+++ hosts/common/users/norpie.nix
@@ -24,6 +24,11 @@
           "kvm"
         ];
         hashedPassword = "$6$yrmuKldtP/mKYReB$9w8pTeEmK/oY2xnY2mG5Wwj6N72qoeoYp.zIHpPS7/inQrR70LU0dNvjUFc0hTFvS9BcwUV9XY2.girsF8JyN.";
+        openssh = {
+          authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICYMKJo0UVB8Pl0ZfQAe9XLwhHSKfaCzP4ylZQqqFnGG"
+          ];
+        };
       };
 
       root = {
