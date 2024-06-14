3b3c95e951cc12d49cd45cb48b42c1d0f74702c0
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-06-14 17:35:52 +0200

    feat: added smartd and smartmontools

--- hosts/common/core/services.nix
+++ hosts/common/core/services.nix
@@ -1,5 +1,9 @@
-{ ... }: {
+{pkgs, ...}: {
   services.fstrim.enable = true;
   services.upower.enable = true;
   services.getty.autologinUser = "norpie";
+  services.smartd.enable = true;
+  environment.systemPackages = with pkgs; [
+    smartmontools
+  ];
 }
