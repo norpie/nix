612b94e8a72c2ea9c0629fc46ab96eb76b7c1bb1
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-06-26 17:40:17 +0200

    feat: switched to grub and added a theme

--- hosts/common/core/boot.nix
+++ hosts/common/core/boot.nix
@@ -5,9 +5,21 @@
       "amd_iommu=on"
     ];
     loader = {
-      systemd-boot.enable = true;
+      systemd-boot.enable = false;
+      grub = {
+        enable = true;
+        devices = ["nodev"];
+        efiSupport = true;
+        useOSProber = true;
+        theme = pkgs.fetchFromGitHub {
+          owner = "shvchk";
+          repo = "fallout-grub-theme";
+          rev = "80734103d0b48d724f0928e8082b6755bd3b2078";
+          sha256 = "sha256-7kvLfD6Nz4cEMrmCA9yq4enyqVyqiTkVZV5y4RyUatU=";
+        };
+      };
       efi.canTouchEfiVariables = true;
-      timeout = 3;
+      timeout = 5;
     };
   };
   environment.systemPackages = with pkgs; [
