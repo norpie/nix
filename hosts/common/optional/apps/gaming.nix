bcc193ecfcc6d8c26c77cfd762bb5326b7ddccaf
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-07-10 20:06:44 +0200

    feat(gaming): enable xpadneo

--- hosts/common/optional/apps/gaming.nix
+++ hosts/common/optional/apps/gaming.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   # TODO: This v
   # https://github.com/mateusauler/nixos-config/blob/9234534867e50cef659997c67a5b71eb84e28670/home-manager-modules/scripts/steam-xdg.nix
+  hardware.xpadneo.enable = true;
   programs = {
     gamemode.enable = true;
     steam = {
