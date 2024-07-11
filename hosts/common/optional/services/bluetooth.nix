83295115569376a2b7e267cd0acb4e9ddb8d1fb7
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-07-12 00:43:43 +0200

    feat(bluetooth): added some more bluetooth settings

--- hosts/common/optional/services/bluetooth.nix
+++ hosts/common/optional/services/bluetooth.nix
@@ -1,5 +1,13 @@
 {...}: {
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        ControllerMode = "dual";
+        JustWorksRepairing = true;
+      };
+    };
+  };
   services.blueman.enable = true;
 }
