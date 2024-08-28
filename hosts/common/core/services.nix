5658717b68c284a44b4fff285ddbc79c3e038302
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-08-28 09:56:11 +0200

    feat: autoUpgrade

--- hosts/common/core/services.nix
+++ hosts/common/core/services.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   services.fstrim.enable = true;
   services.upower.enable = true;
   services.getty.autologinUser = "norpie";
@@ -6,4 +10,16 @@
   environment.systemPackages = with pkgs; [
     smartmontools
   ];
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "--no-write-lock-file"
+      "-L" # print build logs
+    ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
+  };
 }
