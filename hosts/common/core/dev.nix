d5ae2314f00dd8d7b67bce27402a090e69fbf705
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-08-31 23:23:49 +0200

    feat: added rust-analyzer, and reorganized

--- hosts/common/core/dev.nix
+++ hosts/common/core/dev.nix
@@ -10,21 +10,37 @@
     websocat
 
     # Language servers
-    clang-tools_18
+    # c/c++
+    clang-tools
+    # css
     emmet-ls
+    # go
     gopls
+    # lua
     lua-language-server
+    #
     nil
+    # bash
     nodePackages_latest.bash-language-server
+    # python
     pyright
+    # typescript
     nodePackages_latest.typescript-language-server
+    # svelte
     nodePackages_latest.svelte-language-server
+    # html, css, js
     vscode-langservers-extracted
-    ra-multiplex
+    # toml
     taplo
+    # latex
     texlab
+    # yaml
     yaml-language-server
+    # json
     zls
+    # rust
+    ra-multiplex
+    rust-analyzer
 
     # Formatters
     alejandra
@@ -33,21 +49,23 @@
     black
     prettierd
 
-    # Libs
-    libsodium
-    openssl
-
-    # Language utilities
+    # compilers
     clang
-    jq
-    gcc13
+    gcc
+
+    # linkers
     mold
+
+    # package managers
+    cargo
+
+    # cli tools
+    jq
 
     # Languages
     go
     nodePackages_latest.nodejs
     nodePackages_latest.npm
-    cargo
     python3
     python310
   ];
