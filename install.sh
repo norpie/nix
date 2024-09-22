4cc3bea8520aec1a70dcfb308741fc8b286f3960
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-09-23 00:09:08 +0200

    fix: use nix-store at real drive

--- install.sh
+++ install.sh
@@ -363,7 +363,7 @@ fi
 
 # Install NixOS
 normal "Installing NixOS"
-nom build --extra-experimental-features "nix-command flakes" .#nixosConfigurations.$hostname.config.system.build.toplevel
+nom build --extra-experimental-features "nix-command flakes" --store /mnt/nix/store /mnt .#nixosConfigurations.$hostname.config.system.build.toplevel
 nixos-install --impure --flake ".#$hostname" --root /mnt --no-root-passwd
 if [ $? -ne 0 ]; then
     error "Failed to install NixOS"
