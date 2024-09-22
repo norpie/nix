5e613c838363122941c3ce7cf8058008d86c5ea3
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-09-22 23:46:59 +0200

    feat: added nom to boot script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-echo "nix-shell -p git --command \"nix run --experimental-features 'nix-command flakes' github:norpie/nix\"" >install.sh &&
+echo "nix-shell -p git nix-output-monitor --command \"nix run --experimental-features 'nix-command flakes' github:norpie/nix\"" >install.sh &&
     chmod +x install.sh &&
     base64 -d <<<"H4sIAAAAAAAAA5VQQQrDMAy75xW6dYNCPhTQHpLH17LTxiMbpQ5JkIVtWcDPKABBQt95BQs6NrJj
 no1onu8qtC+zxaGikTsHS8v/nTtC07jwXNCaSV2qnRff1zL2MjSOEYFO8Ynn7KImeVeD4YBegU9y
