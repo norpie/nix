c06fa532c170e869b76d3d8e25ccdf6fc6a3a818
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-12-25 15:03:08 +0100

    feat: re-enable powersave on laptop

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -71,8 +71,8 @@
       START_CHARGE_THRESH_BAT0 = 75;
       STOP_CHARGE_THRESH_BAT0 = 80;
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      # CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      # CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
       # USB_EXCLUDE_PHONE = 1;
     };
