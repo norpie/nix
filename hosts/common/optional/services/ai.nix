76812261dafeda0a1eb07904010e6a04981ef1d2
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-10 12:33:27 +0100

    feat: disable ai until rocm fix

--- hosts/common/optional/services/ai.nix
+++ hosts/common/optional/services/ai.nix
@@ -1,12 +1,13 @@
 {...}: {
     services = {
         ollama = {
-            enable = true;
+            enable = false;
             host = "0.0.0.0";
             home = "/home/norpie/persist/ai/ollama-state";
+            acceleration = false;
         };
         open-webui = {
-            enable = true;
+            enable = false;
             host = "0.0.0.0";
             port = 3000;
             stateDir = "/home/norpie/persist/ai/open-webui-state";
