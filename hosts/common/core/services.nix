47b3fff2ee0defd9fbb27e948875f1badd09e504
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-11 12:00:02 +0100

    feat: disabled auto upgrade

--- hosts/common/core/services.nix
+++ hosts/common/core/services.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  inputs,
-  ...
-}: {
+{pkgs, ...}: {
   services.fstrim.enable = true;
   services.upower.enable = true;
   services.getty.autologinUser = "norpie";
@@ -10,16 +6,4 @@
   environment.systemPackages = with pkgs; [
     smartmontools
   ];
-  system.autoUpgrade = {
-    enable = true;
-    flake = inputs.self.outPath;
-    flags = [
-      "--update-input"
-      "nixpkgs"
-      "--no-write-lock-file"
-      "-L" # print build logs
-    ];
-    dates = "02:00";
-    randomizedDelaySec = "45min";
-  };
 }
