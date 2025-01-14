6f63ee684ab4bbe6ba6385e594fded20699b980d
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-14 11:51:46 +0100

    refactor: clear up audio services

--- hosts/common/optional/services/audio.nix
+++ hosts/common/optional/services/audio.nix
@@ -6,28 +6,33 @@
   services.pipewire = {
     enable = true;
 
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
 
-    pulse.enable = true;
-    wireplumber.enable = true;
+    pulse = {
+      enable = true;
+    };
+
+    wireplumber = {
+      enable = true;
+      configPackages = [
+        (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
+          bluez_monitor.properties = {
+                  ["bluez5.enable-sbc-xq"] = true,
+                  ["bluez5.enable-msbc"] = true,
+                  ["bluez5.codecs"] = "[ sbc_xq ]",
+                  ["bluez5.roles"] = "[ a2dp_sink ]",
+                  ["bluez5.hfphsp-backend"] = "none"
+          }
+        '')
+      ];
+    };
   };
   environment.systemPackages = with pkgs; [
     pulseaudio
     pamixer
     pavucontrol
-  ];
-
-  # Wireplumber bluetooth config
-  services.pipewire.wireplumber.configPackages = [
-    (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
-      bluez_monitor.properties = {
-              ["bluez5.enable-sbc-xq"] = true,
-              ["bluez5.enable-msbc"] = true,
-              ["bluez5.codecs"] = "[ sbc_xq ]",
-              ["bluez5.roles"] = "[ a2dp_sink ]",
-              ["bluez5.hfphsp-backend"] = "none"
-      }
-    '')
   ];
 }
