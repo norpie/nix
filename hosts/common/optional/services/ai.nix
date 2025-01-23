32560b5dcea7386b2fed32bd22c50ec8b6fd92d3
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-23 21:24:43 +0100

    feat: change ollama package and format everything in ai

--- hosts/common/optional/services/ai.nix
+++ hosts/common/optional/services/ai.nix
@@ -1,19 +1,20 @@
-{...}: {
-    services = {
-        ollama = {
-            enable = false;
-            host = "0.0.0.0";
-            home = "/home/norpie/persist/ai/ollama-state";
-            acceleration = false;
-        };
-        open-webui = {
-            enable = false;
-            host = "0.0.0.0";
-            port = 3000;
-            stateDir = "/home/norpie/persist/ai/open-webui-state";
-        };
-        tabby = {
-            enable = false;
-        };
+{pkgs, ...}: {
+  services = {
+    ollama = {
+      enable = false;
+      package = pkgs.ollama-rocm;
+      host = "0.0.0.0";
+      home = "/home/norpie/persist/ai/ollama-state";
+      acceleration = false;
     };
+    open-webui = {
+      enable = false;
+      host = "0.0.0.0";
+      port = 3000;
+      stateDir = "/home/norpie/persist/ai/open-webui-state";
+    };
+    tabby = {
+      enable = false;
+    };
+  };
 }
