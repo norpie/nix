6f09e28d2f28f393272c9d5fbb8066ac9b77113e
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-04 19:57:10 +0100

    feat: added core dev stuff to wsl, removed old stuff

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -1,51 +1,19 @@
 {
   pkgs,
+  configLib,
   inputs,
   ...
 }: {
   imports = [
     inputs.nixos-wsl.nixosModules.wsl
+
+    (configLib.relativeToRoot "hosts/common/core/dev")
+    (configLib.relativeToRoot "hosts/common/core/nix")
+    (configLib.relativeToRoot "hosts/common/core/packages")
+    (configLib.relativeToRoot "hosts/common/core/shell")
   ];
 
-  wsl = {
-    defaultUser = "norpie";
+  wsl.enable = true;
 
-    enable = true;
-    wslConf.automount.root = "/mnt";
-  };
-
-  # WSL is closer to a container than anything else
-  boot.isContainer = true;
-
-  networking.hostName = "wsl";
-
-  environment.etc.hosts.enable = false;
-  environment.etc."resolv.conf".enable = false;
-
-  networking.dhcpcd.enable = false;
-
-  users.users.root = {
-    # Otherwise WSL fails to login as root with "initgroups failed 5"
-    extraGroups = ["root"];
-  };
-
-  security.sudo.wheelNeedsPassword = false;
-
-  # Disable systemd units that don't make sense on WSL
-  systemd.services."serial-getty@ttyS0".enable = false;
-  systemd.services."serial-getty@hvc0".enable = false;
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@".enable = false;
-
-  systemd.services.firewall.enable = false;
-  systemd.services.systemd-resolved.enable = false;
-  systemd.services.systemd-udevd.enable = false;
-
-  # Don't allow emergency mode, because we don't have a console.
-  systemd.enableEmergencyMode = false;
-
-  environment.systemPackages = with pkgs; [
-    # Required for WSL
-    wslu
-  ];
+  system.stateVersion = "24.05";
 }
