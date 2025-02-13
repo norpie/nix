8be68ce2c04ae50a3989309c38c9283999c967e8
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-13 08:49:15 +0100

    feat: added hosts entry for vpn ip

--- hosts/common/core/network.nix
+++ hosts/common/core/network.nix
@@ -1,6 +1,11 @@
 {...}: {
-  networking.networkmanager.enable = true;
-  networking.firewall.enable = false;
+  networking = {
+    networkmanager.enable = true;
+    firewall.enable = false;
+    hosts = {
+      "10.147.17.202" = ["home.norpie.dev"];
+    };
+  };
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
