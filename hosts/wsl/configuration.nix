15e037ae588107cd60c783ad433bc2f356304de1
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-03-14 09:03:52 +0100

    feat: added syncthing to wsl

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -25,4 +25,33 @@
   environment.systemPackages = with pkgs; [
     zerotierone
   ];
+
+  services = {
+    syncthing = {
+      guiAddress = "127.0.0.1:8384";
+      enable = true;
+      user = "norpie";
+      dataDir = "/home/norpie"; # Default folder for new synced folders
+      configDir = "/home/norpie/.config/syncthing"; # Folder for Syncthing's settings and keys
+      settings = {
+        gui = {
+          address = "127.0.0.1:8384";
+          password = "password";
+          theme = "dark";
+        };
+        devices = {
+          "jupiter" = {
+            id = "ANBXVIC-K2YZPTG-AB5JCBW-QOVMTGC-TVZXWIE-GA6MZWS-HH6B4YX-77ON5QV";
+          };
+        };
+        folders = {
+          "work" = {
+            id = "za4mr-vatzw";
+            path = "/home/norpie/repos";
+            devices = [ "jupiter" ];
+          };
+        };
+      };
+    };
+  };
 }
