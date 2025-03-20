e891c01bd25b557f760d2c8ac6ce92086d533f92
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-03-20 08:09:30 +0000

    feat: make wsl syncthing listen global

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -28,14 +28,14 @@
 
   services = {
     syncthing = {
-      guiAddress = "127.0.0.1:8384";
+      guiAddress = "0.0.0.0:8384";
       enable = true;
       user = "norpie";
       dataDir = "/home/norpie"; # Default folder for new synced folders
       configDir = "/home/norpie/.config/syncthing"; # Folder for Syncthing's settings and keys
       settings = {
         gui = {
-          address = "127.0.0.1:8384";
+          address = "0.0.0.0:8384";
           password = "password";
           theme = "dark";
         };
