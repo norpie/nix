585bf9e2e28a32cc20387986aaefe202cb1bbeeb
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-05-02 10:06:19 +0200

    feat: config vaultwarden ip

--- hosts/common/optional/services/vaultwarden.nix
+++ hosts/common/optional/services/vaultwarden.nix
@@ -2,5 +2,9 @@
     services.vaultwarden = {
         enable = true;
         backupDir = "/mnt/data/data/vaultwarden";
+        config = {
+            ROCKET_ADDRESS = "0.0.0.0";
+            ROCKET_PORT = 8222;
+        };
     };
 }
