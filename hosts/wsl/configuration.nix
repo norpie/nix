b86303f4bb4ba4870819d5a0f673e599f01c62de
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-05-15 11:19:55 +0000

    feat: nvidia stuff

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -3,7 +3,34 @@
   configLib,
   inputs,
   ...
-}: {
+}: let
+  wsl-lib = pkgs.runCommand "wsl-lib" {} ''
+    mkdir -p "$out/lib"
+    # # We can't just symlink the lib directory, because it will break merging with other drivers that provide the same directory
+    ln -s /usr/lib/wsl/lib/libcudadebugger.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libcuda.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libcuda.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libcuda.so.1.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libd3d12core.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libd3d12.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libdxcore.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvcuvid.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvcuvid.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvdxdlkernels.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvidia-encode.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvidia-encode.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvidia-ml.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvidia-opticalflow.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvidia-opticalflow.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvoptix.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvwgf2umx.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/nvidia-smi "$out/lib"
+  '';
+in {
+  programs.nix-ld = {
+    enable = true;
+    libraries = [wsl-lib];
+  };
   imports = [
     inputs.nixos-wsl.nixosModules.wsl
 
@@ -17,13 +44,58 @@
   wsl.defaultUser = "norpie";
 
   wsl.wslConf.network.hostname = "wsl";
-
+  wsl.useWindowsDriver = true;
+  # hardware.opengl.setLdLibraryPath = true;
   wsl.docker-desktop.enable = true;
+
+  # virtualisation.docker = {
+  #   enable = true;
+  #   rootless = {
+  #     enable = true;
+  #     setSocketVariable = true;
+  #     daemon.settings = {
+  #       features.cdi = true;
+  #       cdi-spec-dirs = ["/home/norpie/.cdi"];
+  #     };
+  #   };
+  #   daemon.settings = {
+  #     features.cdi = true;
+  #     cdi-spec-dirs = ["/etc/cdi"];
+  #   };
+  # };
+  hardware = {
+    graphics = {
+      enable = true;
+    };
+    nvidia = {
+      # enabled = true;
+      modesetting.enable = true;
+      nvidiaSettings = false;
+      open = false;
+    };
+    nvidia-container-toolkit = {
+      enable = true;
+      mount-nvidia-executables = false;
+    };
+  };
+  services.xserver.videoDrivers = ["nvidia"];
+
+  # services.open-webui = {
+  #     enable = true;
+  # };
+
+  # services.ollama = {
+  #     enable = true;
+  #     acceleration = "cuda";
+  # };
 
   system.stateVersion = "24.05";
 
+  environment.sessionVariables.LD_LIBRARY_PATH = [ "/run/opengl-driver/lib" ];
+
   environment.systemPackages = with pkgs; [
     zerotierone
+    nvidia-container-toolkit
   ];
 
   services = {
@@ -48,7 +120,7 @@
           "work" = {
             id = "za4mr-vatzw";
             path = "/home/norpie/repos";
-            devices = [ "jupiter" ];
+            devices = ["jupiter"];
           };
         };
       };
