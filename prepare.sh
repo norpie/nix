42883257b788e8222216ced947c5b5b2c9ab985b
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-06-02 15:14:18 +0200

    fix: prepare script doesn't fail if binaries not available

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 bin_dir="$(dirname $0)/bins"
 
+if [ -z "$(ls -A "$bin_dir" | grep -v .gitkeep)" ]; then
+    echo "No binaries found in $bin_dir. Exiting."
+    exit 0
+fi
+
 for bin in "$bin_dir"/*; do
     echo "Adding $bin to nix store"
     nix-store --add-fixed sha256 "$bin"
