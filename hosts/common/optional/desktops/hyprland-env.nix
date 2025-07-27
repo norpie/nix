6d687f66fb05022bda1c3365d0ff840a6093a3b9
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-07-27 11:08:28 +0200

    feat: add hyprland stuff

--- hosts/common/optional/desktops/hyprland-env.nix
+++ hosts/common/optional/desktops/hyprland-env.nix
@@ -10,47 +10,18 @@
     (configLib.relativeToRoot "hosts/common/optional/apps/st.nix")
   ];
 
-  # services = {
-  #   displayManager = {
-  #     autoLogin = {
-  #       enable = true;
-  #       user = "norpie";
-  #     };
-  #   };
-  #   picom = {
-  #     enable = true;
-  #   };
-  #   xserver = {
-  #     displayManager = {
-  #       lightdm = {
-  #         enable = true;
-  #         greeter = {
-  #           enable = true;
-  #           name = "lightdm-webkit2-greeter";
-  #         };
-  #       };
-  #     };
-  #     enable = true;
-  #     autorun = true;
-  #     xkb = {
-  #       layout = "us";
-  #       variant = "";
-  #     };
-  #     windowManager = {
-  #       dwm = {
-  #         enable = true;
-  #         package = pkgs.dwm.overrideAttrs {
-  #           src = pkgs.fetchgit {
-  #             name = "dwm";
-  #             url = "https://github.com/norpie/dwm";
-  #             rev = "7b230e9e1e06b7257a30b0b4235407da5ab5d5cc";
-  #             sha256 = "sha256-NWDWiyE4NPiUX0RqLRnZB65pwH+1ymeOJjkUeC1weyA=";
-  #           };
-  #         };
-  #       };
-  #     };
-  #   };
-  # };
+  services = {
+    dbus = {
+      enable = true;
+      implementation = "broker";
+    };
+    displayManager = {
+      autoLogin = {
+        enable = true;
+        user = "norpie";
+      };
+    };
+  };
 
   programs.hyprland = {
     enable = true;
@@ -58,30 +29,9 @@
     xwayland.enable = true;
   };
 
-  # wayland = {
-  #   windowManager = {
-  #     hyprland = {
-  #       enable = true;
-  #       package = pkgs.hyprland;
-  #       xwayland.enable = true;
-  #       systemd.enable = true;
-  #       settings = {
-  #         decoration = {
-  #           shadow_offset = "0 5";
-  #           "col.shadow" = "rgba(00000099)";
-  #         };
-
-  #         "$mod" = "SUPER";
-
-  #         bindm = [
-  #           # mouse movements
-  #           "$mod, mouse:272, movewindow"
-  #           "$mod, mouse:273, resizewindow"
-  #         ];
-  #       };
-  #     };
-  #   };
-  # };
+  programs.waybar = {
+      enable = true;
+  };
 
   # add system packages
   environment.systemPackages = with pkgs; [
@@ -92,15 +42,20 @@
     libnotify
     dunst
 
+    # temrinal
+    alacritty
+
+    # launcher
+    rofi-wayland
+
     # general utilities
     playerctl
 
-    # xorg utilities
-    xorg.xev
-    xclip
-    xdotool
-    xsel
-    xwallpaper
-    numlockx
+    # hypr*
+    hyprpaper
+    hyprlock
+    
+    # cursor theme
+    vanilla-dmz
   ];
 }
