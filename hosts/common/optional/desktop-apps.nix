bc508227b36936039678e9089778d4c5c3e945eb
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-08-02 20:57:14 +0200

    build: update vscode insiders

--- hosts/common/optional/desktop-apps.nix
+++ hosts/common/optional/desktop-apps.nix
@@ -13,7 +13,7 @@
       url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
       # sha256 = lib.fakeHash;
       name = "vsode-insiders-linux-x64-12l49a9wj6g8jffy7gp6qmbias0cqfn94jafkjdwg753gd878c0b";
-      sha256 = "CzB0UHujnMebnE5JkqzDDGgVV8XmvuOdk+gZyZNKhIo=";
+      sha256 = "0a4p10lviclcz5pqv9kk13a4ai71rww3r0013jrhsfnh2jayf3wm";
     };
     version = "latest";
 
