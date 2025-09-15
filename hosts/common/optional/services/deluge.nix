10573cab136a554898c5ef37551ae473299b1ec3
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-09-15 09:20:31 +0200

    feat: changes to deluge and disable i

--- hosts/common/optional/services/deluge.nix
+++ hosts/common/optional/services/deluge.nix
@@ -2,11 +2,11 @@
     services.deluge = {
         enable = true;
         user = "norpie";
-        group = "mediamanager";
-        dataDir = "/home/norpie/.config/deluge"; # Directory for Deluge's configuration and state
+        group = "media";
+        dataDir = "/mnt/data/data/deluge";
         web = {
             enable = true;
-            port = 9999; # Default Deluge Web UI port
+            port = 9999;
         };
     };
 }
