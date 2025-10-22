19c0674f8f13cb2db156a76a2a5efe948dbc8cae
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-10-22 22:57:45 +0200

    feat: gwolves hsk pro stuff

--- hosts/common/optional/gwolves-mouse.nix
+++ hosts/common/optional/gwolves-mouse.nix
@@ -7,8 +7,21 @@
   services.udev.extraRules = ''
     # G-Wolves HSK Pro 8K Receiver (Product ID: 5817)
     # Allows Web HID access for mouse.xyz configuration interface
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="33e4", ATTRS{idProduct}=="5817", MODE="0660", TAG+="uaccess"
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="33e4", ATTRS{idProduct}=="5817", MODE="0660", GROUP="plugdev", TAG+="uaccess"
+
+    # G-Wolves HSK Pro Mouse - Wired mode (Product ID: 5808)
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="33e4", ATTRS{idProduct}=="5808", MODE="0660", GROUP="plugdev", TAG+="uaccess"
+
+    # HID interface permissions for wireless receiver
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="33e4", ATTRS{idProduct}=="5817", MODE="0660", GROUP="plugdev", TAG+="uaccess"
+
+    # HID interface permissions for wired mouse
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="33e4", ATTRS{idProduct}=="5808", MODE="0660", GROUP="plugdev", TAG+="uaccess"
   '';
+
+  # Add plugdev group and add user to it for USB device access
+  users.groups.plugdev = {};
+  users.users.norpie.extraGroups = [ "plugdev" ];
 
   # Note: The web-based software (mouse.xyz) requires a Chromium-based browser
   # Supported browsers: Chrome, Edge, Chromium
