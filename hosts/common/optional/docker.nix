6f149cfc8182f40f75f6b17ec426490a4eb24a3e
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-11-17 14:46:39 +0100

    fix: docker rootless

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
   # hardware.nvidia-container-toolkit.enable = true;
   virtualisation.docker = {
     # package = pkgs.docker_26;
@@ -7,10 +12,26 @@
     rootless = {
       enable = true;
       setSocketVariable = true;
+      daemon.settings = {
+        # Enable buildx by default
+        features = {
+          buildkit = true;
+        };
+      };
+      extraPackages = with pkgs; [
+        docker-compose
+        docker-buildx
+      ];
     };
-    extraPackages = with pkgs; [
-      docker-compose
-      docker-buildx
-    ];
+  };
+
+  # Set up Docker CLI plugins for rootless Docker
+  home-manager.users.norpie = {
+    home.file.".docker/cli-plugins/docker-buildx" = {
+      source = "${pkgs.docker-buildx}/bin/docker-buildx";
+    };
+    home.file.".docker/cli-plugins/docker-compose" = {
+      source = "${pkgs.docker-compose}/bin/docker-compose";
+    };
   };
 }
