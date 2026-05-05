47265d72fb02de8f8e123abfea457480f761153e
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2026-05-05 10:13:58 +0200

    fix: dont force all ssh into tmux to fix remote building

--- hosts/common/optional/services/ssh.nix
+++ hosts/common/optional/services/ssh.nix
@@ -5,12 +5,7 @@
       settings = {
           PermitRootLogin = "no";
           PasswordAuthentication = false;
-          ForceCommand = "tmux new-session -A -s ssh";
       };
-      extraConfig = ''
-        Match User nixremote
-          ForceCommand none
-      '';
   };
   programs.ssh.startAgent = true;
 }
